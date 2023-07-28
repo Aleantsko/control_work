70bac56dc2aec1f78ab028b4423df0b60474e8cf
Aleantsko <135431196+Aleantsko@users.noreply.github.com>
2023-07-28 21:17:35 +0700

    add code C#

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿string[] arr = { "qwerty", "q1we", "asdf" };
+string[] answer = { };
+
+string[] createArray(string[] inputArr)
+{
+    string[] outputArray = { };
+    for (int i = 0; i < inputArr.Length; i++)
+    {
+        if (inputArr[i].Length <= 3)
+        {
+            Array.Resize(ref outputArray, outputArray.Length + 1);
+            outputArray[outputArray.Length - 1] = inputArr[i];
+        }
+    }
+    return outputArray;
+}
+
+void printArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+
+answer = createArray(arr);
+if (answer.Length > 0)
+{
+    printArray(answer);
+}
+else
+{
+    System.Console.WriteLine(" Массив пустой!");
+}
